8021d52dfdcb20a1edc9263e07631a91a5bef3df
James M <jamesmuerle@gmail.com>
2018-04-14 00:45:58 -0700

    Units display on screen.

--- Assets/Scripts/Managers/MapManager.cs
+++ Assets/Scripts/Managers/MapManager.cs
@@ -8,16 +8,20 @@ public class MapManager : MonoBehaviour {
     public int height = 8;
 
     public GameObject floor;
+    public GameObject grunt;
 
     private Transform mapHolder;
+    private Transform unitsHolder;
 
     public void SetupBoard () {
         CreateTiles();
         CenterCamera();
+        AddUnits();
     }
 
-    public void CreateTiles () {
+    private void CreateTiles () {
         mapHolder = new GameObject("Map").transform;
+
         for (int x = 0; x < width; x += 1) {
             for (int y = 0; y < height; y += 1) {
                 GameObject instance =
@@ -27,10 +31,21 @@ public class MapManager : MonoBehaviour {
         }
     }
 
-    private void CenterCamera() {
+    private void CenterCamera () {
         Camera mainCamera = Camera.main;
         mainCamera.orthographicSize = Math.Max(width, height) / 2;
-        // mainCamera.transform.position = new Vector3();
         mainCamera.transform.position = new Vector3(width / 2 - 0.5f, height / 2 - 0.5f, -1);
+    }
+
+    private void AddUnits () {
+        unitsHolder = new GameObject("Units").transform;
+
+        GameObject grunt1 =
+            Instantiate(grunt, new Vector3(2, 2, 0), Quaternion.identity) as GameObject;
+        grunt1.transform.SetParent(unitsHolder);
+
+        GameObject grunt2 =
+            Instantiate(grunt, new Vector3(6, 0, 0), Quaternion.identity) as GameObject;
+        grunt2.transform.SetParent(unitsHolder);
     }
 }
