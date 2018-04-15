9d89e505f116e41068217459334a3fbe7fc6cebd
James M <jamesmuerle@gmail.com>
2018-04-15 03:17:17 -0700

    Fixing lingering moving animation bug when switching selection directly to another unit.

--- Assets/Scripts/Managers/UnitsManager.cs
+++ Assets/Scripts/Managers/UnitsManager.cs
@@ -69,6 +69,7 @@ public class UnitsManager : MonoBehaviour {
         }
         else {
             if (selectedUnit) {
+                selectedUnit.isMoving = false;
                 DeselectUnit();
             }
 
